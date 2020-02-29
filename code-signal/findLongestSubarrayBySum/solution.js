5eb4723a6dd8ea2bac5a6eccb8026839b0321636
Haïder Alshamma <haideralsh@gmail.com>
2020-02-28 20:02:55 -0500

    Add descriptive comment

--- code-signal/findLongestSubarrayBySum/solution.js
+++ code-signal/findLongestSubarrayBySum/solution.js
@@ -1,3 +1,15 @@
+/*
+  Given an array of sorted integers, and a sum 
+  find the longest subarray in the original array that adds up to the given sum.
+
+  Solution implemented in O(n) using 2 pointers p1 and p2
+  iterating forwards creating a "window" that represents the given sum
+  
+  If the sum of the window is less than the given sum, p2 is incremented by 1 
+  else p1 is incremented by 1. When a window adds to the given sum the start and end points are 
+  stored only if they are longer than the longest found yet. 
+*/
+
 const longestSubarrayBySum = (arr, sum) => {
   let p1 = 0,
     p2 = 0,
