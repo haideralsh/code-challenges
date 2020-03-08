92cae91385340db5318298d92425f725d11790a5
Haïder Alshamma <haideralsh@gmail.com>
2020-03-08 13:11:27 -0400

    Move functions into impl

--- leetcode/backspace_compare__844/solution.rs
+++ leetcode/backspace_compare__844/solution.rs
@@ -1,22 +1,33 @@
-fn backspace_compare(s: String, t: String) -> bool {
-  return backspace(s) == backspace(t);
-}
+/*
+Given two strings s and t, return if they are equal when both are typed into empty text editors. # means a backspace character.
 
-fn backspace(s: String) -> String {
-  let mut vec = Vec::new();
+Time complexity: O(n)
+Runtime: 0 ms, faster than 100.00% of Rust online submissions for Backspace String Compare.
+Memory Usage: 2.1 MB, less than 100.00% of Rust online submissions for Backspace String Compare.
 
-  for c in s.chars() { 
-    if c != '#' {
-      vec.push(c.to_string());
-    } else {
-      vec.pop();
+*/
+
+// #[test]
+// fn it_compares_strings_ignoring_backspaces() {
+//  assert_eq!(backspace_compare(String::from("bo#o#oee#"), String::from("##bn#oe")), true);
+// }  
+
+impl Solution {    
+    pub fn backspace_compare(s: String, t: String) -> bool {
+      return Self::backspace(s) == Self::backspace(t);
     }
-  }
 
-  return vec.concat();
-}
+    fn backspace(s: String) -> String {
+      let mut vec = Vec::new();
 
-fn main() {
-  let equal = backspace_compare(String::from("bo#o#oee#"), String::from("##bn#oe"));
-  println!("value of equal: {}", equal);
+      for c in s.chars() { 
+        if c != '#' {
+          vec.push(c.to_string());
+        } else {
+          vec.pop();
+        }
+      }
+
+      return vec.concat();
+    }
 }
