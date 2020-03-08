9ee0ab9678849165e65a40b7e56da5d7cc66a70d
Haïder Alshamma <haideralsh@gmail.com>
2020-03-08 01:11:31 -0500

    Add Rust version for backspace_compare

---
+++ leetcode/backspace_compare__844/solution.rs
@@ -0,0 +1,22 @@
+fn backspace_compare(s: String, t: String) -> bool {
+  return backspace(s) == backspace(t);
+}
+
+fn backspace(s: String) -> std::string::String {
+  let mut vec = Vec::new();
+
+  for c in s.chars() { 
+    if c != '#' {
+      vec.push(c.to_string());
+    } else {
+      vec.pop();
+    }
+  }
+
+  return vec.concat();
+}
+
+fn main() {
+  let equal = backspace_compare(String::from("bo#o#oee#"), String::from("##bn#oe"));
+  println!("value of equal: {}", equal);
+}
